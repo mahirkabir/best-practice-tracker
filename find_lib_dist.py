dbcb2f846c4d46f59a4854bda1aacc25dafa06db
Mahir92 <asefkabir92@gmail.com>
2021-05-04 16:06:01 -0400

    Detect overlap in fixable & unfixable vuls

--- find_lib_dist.py
+++ find_lib_dist.py
@@ -22,6 +22,9 @@ def get_lib_mappings(libs, dict_lib_count, dict_lib_ver_count, dict_severity_cou
             prev_line = lines[i - 1].replace("\n", "").strip()
             lib_info = prev_line.split("  ")
 
+            # if lib_info[0] == "prismjs" and lib_info[1] == "<1.23.0":
+            #     print("")
+
             if lib_info[0] in dict_lib_count:
                 dict_lib_count[lib_info[0]] += 1
                 if lib_info[1] in dict_lib_ver_count[lib_info[0]]:
@@ -34,6 +37,27 @@ def get_lib_mappings(libs, dict_lib_count, dict_lib_ver_count, dict_severity_cou
                 dict_lib_ver_count[lib_info[0]] = {lib_info[1]: 1}
 
     return libs, dict_lib_count, dict_lib_ver_count, dict_severity_count
+
+
+def has_error_in_file(filename):
+    """Check if filename has `npm ERR!` in it"""
+    reader = open(filename, "r", encoding="utf-8")
+    lines = reader.readlines()
+    reader.close()
+    for line in lines:
+        if "npm ERR!" in line:
+            return True
+    return False
+
+
+def fixing_failed(repo):
+    """Return `True` if the `audit fix` or `audit fix --force` operation had `npm ERR!`"""
+    if has_error_in_file(os.path.join("results", "audit_checker",
+                                      repo["name"], "fix_audit_output.txt")):
+        return True
+    else:
+        return has_error_in_file(os.path.join("results", "audit_checker",
+                                              repo["name"], "force_fix_audit_output.txt"))
 
 
 if __name__ == "__main__":
@@ -61,14 +85,22 @@ if __name__ == "__main__":
     dict_lib_count_UNFIXED = {}
     dict_lib_ver_count_UNFIXED = {}
     dict_severity_count_UNFIXED = {}
+
+    fix_failed_repos = []
     for repo in unfixables:
         try:
-            unfixable_libs, dict_lib_count_UNFIXED, dict_lib_ver_count_UNFIXED, dict_severity_count_UNFIXED = get_lib_mappings(
-                unfixable_libs, dict_lib_count_UNFIXED, dict_lib_ver_count_UNFIXED, dict_severity_count_UNFIXED,
-                os.path.join("results", "audit_checker", repo["name"], "after_fix_force.txt"))
+            if fixing_failed(repo):
+                fix_failed_repos.append(repo["name"])
+            else:
+                unfixable_libs, dict_lib_count_UNFIXED, dict_lib_ver_count_UNFIXED, dict_severity_count_UNFIXED = get_lib_mappings(
+                    unfixable_libs, dict_lib_count_UNFIXED, dict_lib_ver_count_UNFIXED, dict_severity_count_UNFIXED,
+                    os.path.join("results", "audit_checker", repo["name"], "after_fix_force.txt"))
 
         except Exception as ex:
             print(ex)
+
+    print("Audit fixing threw errors for %s out of %s unfixable repos: %s\n" % (
+          str(len(fix_failed_repos)), str(len(unfixables)), fix_failed_repos))
 
     """Process Fixable Repos"""
     fixable_libs = []
@@ -91,4 +123,30 @@ if __name__ == "__main__":
         except Exception as ex:
             print(ex)
 
-    print(fixable_libs)
+    overlapped_cnt = 0
+    nonoverlapped_cnt = 0
+    for lib in dict_lib_ver_count_FIXED:
+        if lib in dict_lib_ver_count_UNFIXED:
+            cnt = 0
+            str = ""
+            for ver in dict_lib_ver_count_FIXED[lib]:
+                if ver in dict_lib_ver_count_UNFIXED[lib]:
+                    cnt += 1
+                    str += ver + ",,, "
+                    overlapped_cnt += 1
+                else:
+                    nonoverlapped_cnt += 1
+
+            if str != "":
+                str = lib + ": " + str
+                print(str)
+                print("%s out of %s version of %s had overlap\n" %
+                      (cnt, len(dict_lib_ver_count_FIXED[lib]), lib))
+        else:
+            nonoverlapped_cnt += len(dict_lib_ver_count_FIXED[lib])
+
+    print("%s library versions were both in unfixable and fixable vulneraility list" %
+          overlapped_cnt)
+
+    print("%s library versions were either in unfixable or fixable vulneraility list" %
+          nonoverlapped_cnt)
