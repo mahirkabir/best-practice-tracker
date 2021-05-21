f055d551d351e15705ca1e47f5e37d536c696cf7
Mahir92 <asefkabir92@gmail.com>
2021-05-21 10:19:56 -0400

    Find repos for fixable and unfixable overlaps

--- find_vul_dist.py
+++ find_vul_dist.py
@@ -1,7 +1,8 @@
 import os
 
 
-def get_lib_mappings(libs, dict_lib_count, dict_lib_ver_count, dict_severity_count, filename):
+def get_lib_mappings(libs, dict_lib_count, dict_lib_ver_count,
+                     dict_severity_count, filename, repo, dict_repo_of_lib):
     """Evaluate `filename` and find out updated library version frequency and audit severity frequency"""
 
     reader = open(filename, "r", encoding="utf-8")
@@ -29,14 +30,17 @@ def get_lib_mappings(libs, dict_lib_count, dict_lib_ver_count, dict_severity_cou
                 dict_lib_count[lib_info[0]] += 1
                 if lib_info[1] in dict_lib_ver_count[lib_info[0]]:
                     dict_lib_ver_count[lib_info[0]][lib_info[1]] += 1
+                    dict_repo_of_lib[lib_info[0]][lib_info[1]].append(repo)
                 else:
                     dict_lib_ver_count[lib_info[0]][lib_info[1]] = 1
+                    dict_repo_of_lib[lib_info[0]][lib_info[1]] = [repo]
             else:
                 libs.append(lib_info[0])
                 dict_lib_count[lib_info[0]] = 1
                 dict_lib_ver_count[lib_info[0]] = {lib_info[1]: 1}
+                dict_repo_of_lib[lib_info[0]] = {lib_info[1]: [repo]}
 
-    return libs, dict_lib_count, dict_lib_ver_count, dict_severity_count
+    return libs, dict_lib_count, dict_lib_ver_count, dict_severity_count, dict_repo_of_lib
 
 
 def has_error_in_file(filename):
@@ -58,6 +62,19 @@ def fixing_failed(repo):
     else:
         return has_error_in_file(os.path.join("results", "audit_checker",
                                               repo["name"], "force_fix_audit_output.txt"))
+
+
+def get_overlaps(dict_fixed, dict_unfixed):
+    """Find library versions which are both in fixable and unfixable list"""
+    lib_vers = []
+
+    for lib in dict_fixed:
+        if lib in dict_unfixed:
+            for ver in dict_fixed[lib]:
+                if ver in dict_unfixed[lib]:
+                    lib_vers.append({"lib": lib, "ver": ver})
+
+    return lib_vers
 
 
 if __name__ == "__main__":
@@ -85,6 +102,7 @@ if __name__ == "__main__":
     dict_lib_count_UNFIXED = {}
     dict_lib_ver_count_UNFIXED = {}
     dict_severity_count_UNFIXED = {}
+    dict_repo_of_lib_UNFIXED = {}
 
     fix_failed_repos = []
     for repo in unfixables:
@@ -92,61 +110,75 @@ if __name__ == "__main__":
             if fixing_failed(repo):
                 fix_failed_repos.append(repo["name"])
             else:
-                unfixable_libs, dict_lib_count_UNFIXED, dict_lib_ver_count_UNFIXED, dict_severity_count_UNFIXED = get_lib_mappings(
-                    unfixable_libs, dict_lib_count_UNFIXED, dict_lib_ver_count_UNFIXED, dict_severity_count_UNFIXED,
-                    os.path.join("results", "audit_checker", repo["name"], "after_fix_force.txt"))
+                unfixable_libs, dict_lib_count_UNFIXED, dict_lib_ver_count_UNFIXED, dict_severity_count_UNFIXED, dict_repo_of_lib_UNFIXED = get_lib_mappings(
+                    unfixable_libs, dict_lib_count_UNFIXED,
+                    dict_lib_ver_count_UNFIXED, dict_severity_count_UNFIXED,
+                    os.path.join("results", "audit_checker",
+                                 repo["name"], "after_fix_force.txt"),
+                    repo, dict_repo_of_lib_UNFIXED
+                )
 
         except Exception as ex:
             print(ex)
 
     print("Audit fixing threw errors for %s out of %s unfixable repos: %s\n" % (
-          str(len(fix_failed_repos)), str(len(unfixables)), fix_failed_repos))
+        len(fix_failed_repos), len(unfixables), fix_failed_repos
+    ))
 
     """Process Fixable Repos"""
     fixable_libs = []
     dict_lib_count_FIXED = {}
     dict_lib_ver_count_FIXED = {}
     dict_severity_count_FIXED = {}
+    dict_repo_of_lib_FIXED = {}
+
     for repo in fixables:
         try:
-            if "Vul Found" in repo["initial"]:
-                fixable_libs, dict_lib_count_FIXED, dict_lib_ver_count_FIXED, dict_severity_count_FIXED = get_lib_mappings(
+            if "Vul Found" in repo["initial"] or "Vul Found" in repo["package-lock"]:
+                audit_file_path = os.path.join(
+                    "results", "audit_checker", repo["name"])
+
+                if "Vul Found" in repo["initial"]:
+                    audit_file_path = os.path.join(
+                        audit_file_path, "init_audit.txt")
+                elif "Vul Found" in repo["package-lock"]:
+                    audit_file_path = os.path.join(
+                        audit_file_path, "after_i_lock.txt")
+
+                fixable_libs, dict_lib_count_FIXED, dict_lib_ver_count_FIXED, dict_severity_count_FIXED, dict_repo_of_lib_FIXED = get_lib_mappings(
                     fixable_libs, dict_lib_count_FIXED, dict_lib_ver_count_FIXED, dict_severity_count_FIXED,
-                    os.path.join("results", "audit_checker", repo["name"], "init_audit.txt"))
-            elif "Vul Found" in repo["package-lock"]:
-                fixable_libs, dict_lib_count_FIXED, dict_lib_ver_count_FIXED, dict_severity_count_FIXED = get_lib_mappings(
-                    fixable_libs, dict_lib_count_FIXED, dict_lib_ver_count_FIXED, dict_severity_count_FIXED,
-                    os.path.join("results", "audit_checker", repo["name"], "after_i_lock.txt"))
-            else:
-                pass
+                    audit_file_path, repo, dict_repo_of_lib_FIXED
+                )
 
         except Exception as ex:
             print(ex)
 
-    overlapped_cnt = 0
-    nonoverlapped_cnt = 0
-    for lib in dict_lib_ver_count_FIXED:
-        if lib in dict_lib_ver_count_UNFIXED:
-            cnt = 0
-            str = ""
-            for ver in dict_lib_ver_count_FIXED[lib]:
-                if ver in dict_lib_ver_count_UNFIXED[lib]:
-                    cnt += 1
-                    str += ver + ",,, "
-                    overlapped_cnt += 1
-                else:
-                    nonoverlapped_cnt += 1
+    overlapped_lib_vers = get_overlaps(
+        dict_repo_of_lib_FIXED, dict_repo_of_lib_UNFIXED)
 
-            if str != "":
-                str = lib + ": " + str
-                print(str)
-                print("%s out of %s version of %s had overlap\n" %
-                      (cnt, len(dict_lib_ver_count_FIXED[lib]), lib))
-        else:
-            nonoverlapped_cnt += len(dict_lib_ver_count_FIXED[lib])
+    print("Total overlaps: %s" % len(overlapped_lib_vers))
 
-    print("%s library versions were both in unfixable and fixable vulneraility list" %
-          overlapped_cnt)
+    writer = open(os.path.join(".", "results", "audit_checker",
+                               "audit_fail_checks.txt"), "w", encoding="utf-8")
+    for lib_ver in overlapped_lib_vers:
+        output = ""
+        # output += "============\n"
+        output += "%s\t%s\n" % (lib_ver["lib"], lib_ver["ver"])
 
-    print("%s library versions were either in unfixable or fixable vulneraility list" %
-          nonoverlapped_cnt)
+        # output += "======\n"
+        output += "FIXED\n"
+        for repo in dict_repo_of_lib_FIXED[lib_ver["lib"]][lib_ver["ver"]]:
+            output += "%s\t%s\t%s\t%s\t%s\n" % (
+                repo["name"], repo["initial"], repo["package-lock"], repo["audit-fix"], repo["audit-fix-force"])
+
+        # output += "======\n"
+        output += "UNFIXED\n"
+        for repo in dict_repo_of_lib_UNFIXED[lib_ver["lib"]][lib_ver["ver"]]:
+            output += "%s\t%s\t%s\t%s\t%s\n" % (
+                repo["name"], repo["initial"], repo["package-lock"], repo["audit-fix"], repo["audit-fix-force"])
+
+        # output += "============\n"
+
+        writer.write(output)
+
+    writer.close()
