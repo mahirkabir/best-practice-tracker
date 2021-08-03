249d56aeed596cbbd1151a66cb449302e54320bb
Mahir92 <asefkabir92@gmail.com>
2021-08-03 17:46:04 -0400

    Change audit checking pattern, and keep relevants

--- check_audit.py
+++ check_audit.py
@@ -22,6 +22,8 @@ def get_audit(repo_loc):
             return {"status": constants.AUDIT_TRUE, "info": result[1]}
         else:
             return {"status": constants.AUDIT_FALSE, "info": result[1]}
+    elif ("fix available" in result[1]) or ("Severity" in result[1]):
+        return {"status": constants.AUDIT_FALSE, "info": result[1]}
     elif "npm ERR!" in result[1]:
         # If npm audit failed due to some error, we are treating it as ERR
         return {"status": constants.AUDIT_ERROR, "info": result[1]}
@@ -39,11 +41,6 @@ def fix_audit(repo_loc):
     return helper.execute_cmd(repo_loc, "npm audit fix")
 
 
-def fix_audit_force(repo_loc):
-    """Run `npm audit fix --force` in repo_loc"""
-    return helper.execute_cmd(repo_loc, "npm audit fix --force")
-
-
 if __name__ == "__main__":
     """Check if best practices are used in package.json files"""
 
@@ -55,8 +52,7 @@ if __name__ == "__main__":
     writer = open(os.path.join("results", "audit_checker",
                                "audit_results.txt"), "w", encoding="utf-8")
     writer.write(
-        "Repository\tInitial Audit\tBuild-1\tAfter i package-lock\tBuild-2\tAfter audit fix\tBuild-3" +
-        "\tAfter audit fix --force\tBuild-4")
+        "Repository\tInitial Audit\tAfter i package-lock\tAfter audit fix")
     writer.write("\n")
     writer.close()
 
@@ -87,27 +83,18 @@ if __name__ == "__main__":
                 2. If 1 gives ERR instead of vulnerabilities, install package-lock.json file
                 3. Run `npm audit fix`
                 4. Record `npm audit` result (to check if Step 3 fixed anything)
-                5. Run `npm audit fix --force`
-                6. Record `npm audit` result (to check if Step 5 fixed anything)
                 """
 
                 result = {"name": repo["name"], "initial": "", "package-lock": "",
                           "audit-fix": "", "audit-fix-force": ""}
                 install_lock_output = ["", ""]
                 fix_audit_output = ["", ""]
-                force_fix_audit_output = ["", ""]
-
-                init_build_result = helper.build(repo_loc)
-                after_install_build_result = ["-", ""]
-                after_fix_build_result = ["-", ""]
-                after_force_fix_build_result = ["-", ""]
 
                 init_audit_result = get_audit(repo_loc)
 
                 if init_audit_result["status"] == constants.AUDIT_ERROR:
                     install_lock_output = install_package_lock(repo_loc)
                     after_i_lock_result = get_audit(repo_loc)
-                    after_install_build_result = helper.build(repo_loc)
                 else:
                     # If initial result was not an ERR, we don't do anything for after_i_lock
                     after_i_lock_result = {
@@ -116,7 +103,6 @@ if __name__ == "__main__":
                 if after_i_lock_result["status"] == constants.AUDIT_FALSE:
                     fix_audit_output = fix_audit(repo_loc)
                     after_fix_result = get_audit(repo_loc)
-                    after_fix_build_result = helper.build(repo_loc)
                 elif after_i_lock_result["status"] == constants.AUDIT_ERROR:
                     # If error is still there, then fixing won't solve it
                     after_fix_result = {
@@ -126,19 +112,6 @@ if __name__ == "__main__":
                     after_fix_result = {
                         "status": constants.AUDIT_TRUE, "info": ""}
 
-                if after_fix_result["status"] == constants.AUDIT_FALSE:
-                    force_fix_audit_output = fix_audit_force(repo_loc)
-                    after_fix_force_result = get_audit(repo_loc)
-                    after_force_fix_build_result = helper.build(repo_loc)
-                elif after_fix_result["status"] == constants.AUDIT_ERROR:
-                    # If error is still there, then force fixing won't solve it
-                    after_fix_force_result = {
-                        "status": constants.AUDIT_ERROR, "info": ""}
-                else:
-                    # If previous result was already vulnerability-free, no need to fix anything
-                    after_fix_force_result = {
-                        "status": constants.AUDIT_TRUE, "info": ""}
-
                 result["initial"] = init_audit_result["status"]
                 result["package-lock"] = helper.IFF(
                     init_audit_result["status"] == after_i_lock_result["status"],
@@ -146,23 +119,15 @@ if __name__ == "__main__":
                 result["audit-fix"] = helper.IFF(
                     after_i_lock_result["status"] == after_fix_result["status"],
                     after_fix_result["status"], str(after_fix_result["status"]) + " " + constants.AUDIT_STEP_CHANGED)
-                result["audit-fix-force"] = helper.IFF(
-                    after_fix_result["status"] == after_fix_force_result["status"],
-                    after_fix_force_result["status"], str(after_fix_force_result["status"]) + " " + constants.AUDIT_STEP_CHANGED)
 
                 writer = open(os.path.join("results", "audit_checker",
                                            "audit_results.txt"), "a", encoding="utf-8")
                 writer.write(
-                    "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s" % (
+                    "%s\t%s\t%s\t%s" % (
                         str(result["name"]),
                         str(result["initial"]),
-                        str(init_build_result[0]),
                         str(result["package-lock"]),
-                        str(after_install_build_result[0]),
-                        str(result["audit-fix"]),
-                        str(after_fix_build_result[0]),
-                        str(result["audit-fix-force"]),
-                        str(after_force_fix_build_result[0]),
+                        str(result["audit-fix"])
                     ))
                 writer.write("\n")
                 writer.close()
@@ -176,24 +141,11 @@ if __name__ == "__main__":
                               after_i_lock_result["info"])
                 helper.record(foldername, "after_fix.txt",
                               after_fix_result["info"])
-                helper.record(foldername, "after_fix_force.txt",
-                              after_fix_force_result["info"])
 
                 helper.record(
                     foldername, "install_lock_output.txt", install_lock_output[1])
                 helper.record(foldername, "fix_audit_output.txt",
                               fix_audit_output[1])
-                helper.record(
-                    foldername, "force_fix_audit_output.txt", force_fix_audit_output[1])
-
-                helper.record(foldername, "init_build_result.txt",
-                              init_build_result[1])
-                helper.record(foldername, "after_install_build_result.txt",
-                              after_install_build_result[1])
-                helper.record(foldername, "after_fix_build_result.txt",
-                              after_fix_build_result[1])
-                helper.record(foldername, "after_force_fix_build_result.txt",
-                              after_force_fix_build_result[1])
 
             except Exception as ex:
                 print("Error processing [%s]: %s" % (repo["name"], str(ex)))
