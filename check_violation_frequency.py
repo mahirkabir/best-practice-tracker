f29a65ccd29d32b2589f6988809f74549fafd6bc
Mahir92 <asefkabir92@gmail.com>
2021-08-13 11:37:24 -0400

    Count duplicates in repositories

--- check_violation_frequency.py
+++ check_violation_frequency.py
@@ -75,6 +75,13 @@ if __name__ == "__main__":
     result = {"unused": 0, "duplicate": 0, "vulnerable": 0,
               "duplicate-fixed": 0, "duplicate-part-fixed": 0, "vulnerable-fixed": 0}
 
+    ddp_writer = open(os.path.join("results", "ddp_checker",
+                                   "ddp_count.txt"), "w", encoding="utf-8")
+    ddp_writer.write("Repository\tOriginal Duplicates\n")
+    ddp_writer.close()
+
+    ddp_err_repos = []
+
     for repo in tqdm(repos):
         unused_cnt = count_unused(os.path.join(
             "results", "unused_checker", repo["name"], "unused_pre.txt"))
@@ -107,4 +114,56 @@ if __name__ == "__main__":
         result["vulnerable-fixed"] += int(vulnerable_cnt >
                                           0 and fixed_vulnerable_cnt == 0)
 
+        ddp_writer = open(os.path.join("results", "ddp_checker",
+                                       "ddp_count.txt"), "a", encoding="utf-8")
+        ddp_writer.write("%s\t%s\n" % (repo["name"], duplicate_cnt))
+        ddp_writer.close()
+        if duplicate_cnt == -1:
+            ddp_err_repos.append(repo)
+
     print(result)
+
+    # dataset_path = helper.get_config("PATHS", "DATASET_PATH")
+    # repos = ddp_err_repos
+    # repos_sz = len(repos)
+
+    # range_limit = 50
+    # for i in range(0, repos_sz, range_limit):
+    #     print("Processing repo [%s..%s]:" % (str(i), str(i + range_limit - 1)))
+
+    #     # First clone the (<= range_limit) repositories in dataset folder
+    #     left_repos = min(i + range_limit, repos_sz)
+    #     for j in tqdm(range(i, left_repos)):
+    #         repo = repos[j]
+
+    #         try:
+    #             repo["name"] = helper.clone_repo_to_dir(
+    #                 dataset_path, repo["url"], repo["name"])
+
+    #         except Exception as ex:
+    #             print("Error cloning [%s]: %s" % (repo["name"], str(ex)))
+
+    #     # Process each one of the cloned repositories
+    #     for j in tqdm(range(i, left_repos)):
+    #         repo = repos[j]
+
+    #         repo_loc = os.path.join(dataset_path, repo["name"])
+
+    #         try:
+    #             helper.execute_cmd(repo_loc, "npm i")
+    #             result = helper.execute_cmd(repo_loc, "npm ls --all")
+    #             helper.record(os.path.join("results", "ddp_checker",
+    #                                        repo["name"]), "before_ddp.txt", result[1])
+    #         except Exception as ex:
+    #             print("Error processing [%s]: %s" % (repo["name"], str(ex)))
+
+    #     # Delete the (<= range_limit) repositories in dataset folder
+    #     for j in tqdm(range(i, left_repos)):
+    #         repo = repos[j]
+
+    #         try:
+    #             helper.remove_folder(
+    #                 dataset_path, repo["name"])
+
+    #         except Exception as ex:
+    #             print("Error deleting [%s]: %s" % (repo["name"], str(ex)))
